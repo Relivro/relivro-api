71ec0474d54bd40fa3270f28bfdc5c6eb0c4fbac
Danrley Senegalha Pires <dan.osp@outlook.com>
2024-01-12 20:39:50 -0300

    chore: creates project

---
+++ src/main/java/com/api/relivro/RelivroApplication.java
@@ -0,0 +1,13 @@
+package com.api.relivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelivroApplication.class, args);
+	}
+
+}
