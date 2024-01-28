e4cffaf8b25b9ff3d24d6820d63812bb707eb1f8
Danrley Senegalha Pires <dan.osp@outlook.com>
2024-01-28 09:57:41 -0300

    chore: rename dir model

--- src/main/java/com/api/relivro/model/UserModel.java
+++ src/main/java/com/api/relivro/model/UserModel.java
@@ -1,4 +1,4 @@
-package com.api.relivro.models;
+package com.api.relivro.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/com/api/relivro/repository/UserRepository.java
+++ src/main/java/com/api/relivro/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.api.relivro.repository;
 
-import com.api.relivro.models.UserModel;
+import com.api.relivro.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
